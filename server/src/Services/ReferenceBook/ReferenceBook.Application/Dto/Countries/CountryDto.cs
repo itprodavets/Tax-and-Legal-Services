efe6797a8ad894cbe9eaf683c98da5227ad3d08f
Denis Kharchenko <itprodavets@hotmail.com>
2019-12-14 14:03:21 +0800

    fixed nullable reference type

--- server/src/Services/ReferenceBook/ReferenceBook.Application/Dto/Countries/CountryDto.cs
+++ server/src/Services/ReferenceBook/ReferenceBook.Application/Dto/Countries/CountryDto.cs
@@ -2,11 +2,11 @@ namespace ReferenceBook.Application.Dto.Countries
 {
 	public class CountryDto
 	{
-		public string Name { get; }
-		public string Alpha2Code { get; }
-		public string Alpha3Code { get; }
-		public short NumericCode { get; }
-		public string Region { get; }
-		public string SubRegion { get; }
+		public string Name { get; } = string.Empty;
+		public string Alpha2Code { get; } = string.Empty;
+		public string Alpha3Code { get; } = string.Empty;
+		public short NumericCode { get; } = 0;
+		public string Region { get; } = string.Empty;
+		public string SubRegion { get; } = string.Empty;
 	}
 }
--- server/src/Services/ReferenceBook/ReferenceBook.Application/Dto/Languages/LanguageDto.cs
+++ server/src/Services/ReferenceBook/ReferenceBook.Application/Dto/Languages/LanguageDto.cs
@@ -2,8 +2,8 @@ namespace ReferenceBook.Application.Dto.Languages
 {
 	public class LanguageDto
 	{
-		public string Name { get; }
-		public string NativeName { get; }
-		public string Code { get; }
+		public string Name { get; } = string.Empty;
+		public string NativeName { get; } = string.Empty;
+		public string Code { get; } = string.Empty;
 	}
 }
