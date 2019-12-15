61c33b87097a7e10857e52dde6b13e88dc1fb23a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-12-15 15:18:54 +0300

    format ReferenceBookContext

--- server/src/Services/ReferenceBook/ReferenceBook.Infrastructure/ReferenceBookContext.cs
+++ server/src/Services/ReferenceBook/ReferenceBook.Infrastructure/ReferenceBookContext.cs
@@ -4,15 +4,15 @@ using ReferenceBook.Domain.AggregatesModel.LanguageAggregate;
 
 namespace ReferenceBook.Infrastructure
 {
-	public class ReferenceBookContext : DbContext
-	{
-		public DbSet<Country> Countries { get; set; } = null!;
-		public DbSet<Language> Languages { get; set; } = null!;
-		
-		public ReferenceBookContext(DbContextOptions<ReferenceBookContext> options) : base(options)
-		{
-		}
-		
-		protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-	}
+    public class ReferenceBookContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Language> Languages { get; set; } = null!;
+
+        public ReferenceBookContext(DbContextOptions<ReferenceBookContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
 }
