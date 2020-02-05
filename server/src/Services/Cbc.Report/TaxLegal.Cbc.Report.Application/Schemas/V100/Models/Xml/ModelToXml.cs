1f380e37fbd656e4852b33d96e6a77c11782d56d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-02-05 22:56:09 +0300

    Add blank ModelToXml.Convert implementations

---
+++ server/src/Services/Cbc.Report/TaxLegal.Cbc.Report.Application/Schemas/V100/Models/Xml/ModelToXml.cs
@@ -0,0 +1,13 @@
+using System;
+using TaxLegal.Cbc.Report.Application.Dto;
+
+namespace TaxLegal.Cbc.Report.Application.Schemas.V100.Models.Xml
+{
+    public static class ModelToXml
+    {
+        public static CBC_OECD Convert(ReportData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
